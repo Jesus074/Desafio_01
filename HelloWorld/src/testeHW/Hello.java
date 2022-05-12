864c1801b46e854d3ea7e70f75c054dd1d17c14a
Jesus074 <aleflima2314@gmail.com>
2022-05-12 00:29:18 -0300

    Desafio 01 completo - Alef

---
+++ HelloWorld/src/testeHW/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package testeHW;
+/**
+ * @author ADM
+ *
+ */
+public class Hello {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+}
